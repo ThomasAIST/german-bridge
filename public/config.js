75ff9f6540c5b401b84a02e494860521e71335d1
ThomasAIST <lokhin.y@gmail.com>
2026-09-08 01:08:01 +0800

    Fix API URL

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // Set API_URL to the public URL of the Node/Socket.IO server when using GitHub Pages.
 window.GB_CONFIG = {
-  API_URL: 'ec2-54-252-248-255.ap-southeast-2.compute.amazonaws.com',
+  API_URL: 'https://ec2-54-252-248-255.ap-southeast-2.compute.amazonaws.com',
 };
