d30e7cbed7a4c8786d9a97d7060ad949f5f33522
ThomasAIST <lokhin.y@gmail.com>
2026-09-08 22:48:20 +0800

    Switch from cors proxy to cloudfront

--- public/config.js
+++ public/config.js
@@ -1,7 +1,6 @@
-// The proxy must receive the complete backend URL in its `url` query value.
+// CloudFront provides HTTPS and forwards REST and Socket.IO traffic to EC2.
 window.GB_CONFIG = {
-  API_PROXY_URL: 'https://corsproxy.io/?key=dfa5a403&url=',
-  API_TARGET_URL: 'http://ec2-54-252-248-255.ap-southeast-2.compute.amazonaws.com:3000',
-  // Socket.IO needs a WebSocket-capable HTTPS endpoint; corsproxy.io is not one.
-  SOCKET_URL: '',
+  API_PROXY_URL: '',
+  API_TARGET_URL: 'https://d1nhpi811j59e0.cloudfront.net',
+  SOCKET_URL: 'https://d1nhpi811j59e0.cloudfront.net',
 };
