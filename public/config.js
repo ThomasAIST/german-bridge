f03d532488a413636ba8b4b286523f4aac34f25b
ThomasAIST <lokhin.y@gmail.com>
2026-09-08 16:08:28 +0800

    API URL to HTTPS

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // Direct EC2 testing endpoint. Use HTTPS on port 443 in production.
 window.GB_CONFIG = {
-  API_URL: 'http://ec2-54-252-248-255.ap-southeast-2.compute.amazonaws.com:3000',
+  API_URL: 'https://ec2-54-252-248-255.ap-southeast-2.compute.amazonaws.com:3000',
 };
